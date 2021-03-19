aa2e1bb232bf9a06bebff65679d9a9fdf2a931d8
sonal <sonal.codexive@gmail.com>
2021-03-19 17:02:43 +0530

    new UI update

--- src/app/tabs/baraqahtab/baraqahaccordion/baraqahaccordion.component.ts
+++ src/app/tabs/baraqahtab/baraqahaccordion/baraqahaccordion.component.ts
@@ -8,6 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class BaraqahaccordionComponent implements OnInit {
 
   constructor() { }
+  selectedItem=-1;
+  priceContent = [
+    {
+      price: "+£0.30"
+    },
+    {
+      price: "+£0.40"
+    },
+    {
+      price: "+£0.50"
+    },
+    {
+      price: "+£0.60"
+    }
+  ]
+
+
   toggle1: boolean = false;
   clicktoggle1(){
     this.toggle1 = !this.toggle1;       
@@ -16,15 +33,15 @@ export class BaraqahaccordionComponent implements OnInit {
   clicktoggle2(){
     this.toggle2 = !this.toggle2; 
   }
-  toggle3: boolean = false;
+  toggle3: boolean = true;
   clicktoggle3(){
     this.toggle3 = !this.toggle3; 
   }
-  toggle4: boolean = false;
+  toggle4: boolean = true;
   clicktoggle4(){
     this.toggle4 = !this.toggle4; 
   }
-  toggle5: boolean = false;
+  toggle5: boolean = true;
   clicktoggle5(){
     this.toggle5 = !this.toggle5; 
   }
--- src/app/tabs/sharetab/sharetab.component.ts
+++ src/app/tabs/sharetab/sharetab.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
 @Component({
   selector: 'app-sharetab',
   templateUrl: './sharetab.component.html',
@@ -6,14 +7,80 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class SharetabComponent implements OnInit {
   @Output() onTabClick: EventEmitter<any> = new EventEmitter();
+
   constructor() { }
+  selectedItem=-1;
+  content = [
+    {
+      name: "Personalised e-Card",
+      image: "./assets/images/personalised-e-card.png",
+      price: "Free",
+      bgcolor: "#ab6cad"
+    },
+    {
+      name: "Postcard",
+      image: "./assets/images/postcard.png",
+      price: "£1.99",
+      bgcolor: "#e6557f"
+    },
+    {
+      name: "Box of Chocolates",
+      image: "./assets/images/boxes-of-chocolates.png",
+      price: "£4.99",
+      bgcolor: "#2c8fcd"
+    },
+    {
+      name: "Personalised Salah Mat",
+      image: "./assets/images/personalised-salah-mat.png",
+      price: "£14.95",
+      bgcolor: "#0ea08f"
+    },
+    {
+      name: "Dates",
+      image: "./assets/images/dates.png",
+      price: "£11.95",
+      bgcolor: "#F79D11"
+    },
+    {
+      name: "Zam Zam Water",
+      image: "./assets/images/zam-zam-water.png",
+      price: "£7.99",
+      bgcolor: "#ab6cad"
+    }
+  ];
+  dveliverySelectedItem=-1;
+  dveliveryContent = [
+    {
+      name: "Royal Mail Special Delivery Guaranteed by 1pm®",
+      text: "Guaranteed by 1pm next-day",
+      price: "6.75"
+    },
+    {
+      name: "Royal Mail Signed For® 1st Class",
+      text: "Signature on delivery and next-day delivery aim",
+      price: "2.25"
+    },
+    {
+      name: "Royal Mail 1st Class",
+      text: "Next-day delivery aim for letters and parcels",
+      price: "2.25"
+    },
+    {
+      name: "Royal Mail 2nd Class",
+      text: "Delivery in two to three working days",
+      price: "0.66"
+    }
+  ];
+  selectedToday=-1;
 
   ngOnInit(): void {
   }
+
   goToNextStep(){
     this.onTabClick.emit("Baraqah");
   }
   goToBackStep(){
     this.onTabClick.emit("Gift");
   }
+
 }
